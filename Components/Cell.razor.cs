d5888fc9a4c03ab9630375098e3627554edcd90a
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-01 14:58:45 +0200

    split @code in dedicated cs file

---
+++ Components/Cell.razor.cs
@@ -0,0 +1,32 @@
+using blazorEx.Messages;
+using blazorEx.Models.Entities;
+using blazorEx.Tools;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace blazorEx.Components;
+public partial class Cell
+{
+    [Parameter]
+    public CaseInfo? CurrentCase { get; set; }
+
+    private void FlagCell(MouseEventArgs e)
+    {
+        if (CurrentCase is not null)
+        {
+            if (!CurrentCase.IsOpen)
+                CurrentCase.IsFlagged = !CurrentCase.IsFlagged;
+        }
+    }
+    private void OpenCell()
+    {
+        if (CurrentCase is not null)
+        {
+            if (!CurrentCase.IsFlagged)
+            {
+                CurrentCase.IsOpen = true;
+                Mediator<ClickMessage>.Instance.Notify(new ClickMessage(CurrentCase.Position));
+            }
+        }
+    }
+}
