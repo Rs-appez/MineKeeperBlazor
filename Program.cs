6202f839f63229fe09b79766e416e0c900e35b5d
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-01 15:57:19 +0200

    add http client

--- Program.cs
+++ Program.cs
@@ -8,4 +8,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddHttpClient(ApiName.DinoApi, options =>
+{
+    options.BaseAddress = new Uri("https://localhost:5192/");
+});
+
 await builder.Build().RunAsync();
+
+public static class ApiName
+{
+    public const string DinoApi = "DinoApi";
+}
