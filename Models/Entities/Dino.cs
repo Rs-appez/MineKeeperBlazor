2147cde1b6cc044ca70d0c987941b34c68e7f63c
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-01 16:35:44 +0200

    add dino Entity

---
+++ Models/Entities/Dino.cs
@@ -0,0 +1,9 @@
+namespace blazorEx.Models.Entities;
+public class Dino
+{
+    public int Id { get; set; }
+    public string? Espece { get; set; }
+    public float LenghtMeters { get; set; }
+    public float WeightKg { get; set; }
+
+}
