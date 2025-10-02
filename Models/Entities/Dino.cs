0d65427e1ad1bb8d7138f23fcb25e4b10b5de6a4
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-02 10:52:08 +0200

    fix typo

--- Models/Entities/Dino.cs
+++ Models/Entities/Dino.cs
@@ -3,7 +3,7 @@ public class Dino
 {
     public int Id { get; set; }
     public string? Espece { get; set; }
-    public float LenghtMeters { get; set; }
+    public float LengthMeters { get; set; }
     public float WeightKg { get; set; }
 
 }
